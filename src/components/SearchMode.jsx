eb276b3f3e8cbb1ba5a0ddbfbee5a60970000e4c
guillaume drevo <guillaume.drevo@gmail.com>
2023-04-23 22:11:37 +0200

    searchmode part almost complete, still need to take care of the showmeal view

--- src/components/SearchMode.jsx
+++ src/components/SearchMode.jsx
@@ -2,26 +2,45 @@ import { useState } from "react";
 import Results from "./Results";
 import SearchBar from "./SearchBar";
 import axios from 'axios';
+import ShowView from "./ShowView";
 
 export default function SearchMode() {
   const [data, setData] = useState([])
   const [input, setInput] = useState('')
+  const [selectedRecipe, setSelectedRecipe] = useState(null)
 
   const updateInput = (e) => {
     setInput(e.target.value)
   }
 
   const handleSubmit = (input) => {
-    console.log(input)
     axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input}`)
       .then(response => {
         setData(response.data);
-        console.log(response.data)
       })
       .catch(error => {
         console.error(error);
       });
   }
+
+  const selectRecipe = (id) => {
+    const recipe = data.meals.find((meal) => meal.idMeal === id)
+    setSelectedRecipe(recipe)
+  }
+
+  const clearSelectedRecipe = () => {
+    setSelectedRecipe(null)
+  }
+
+  const results = <Results
+                    data={data}
+                    selectRecipe={selectRecipe}
+                  />
+
+  const showView =  <ShowView
+                      data={selectedRecipe}
+                      handleClick={clearSelectedRecipe}
+                    />
 
   return(
     <div className="SearchMode">
@@ -30,9 +49,7 @@ export default function SearchMode() {
         input={input}
         handleChange={updateInput}
       />
-      <Results
-        data={data}
-      />
+      {selectedRecipe === null ? results : showView}
     </div>
   )
 }
